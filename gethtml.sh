3ebc3a3e1c9bfa989ec206d6510d69ef9bf8ab37
fngoo <38164287+fngoo@users.noreply.github.com>
2019-10-15 09:05:34 +0800

    Update gethtml.sh

--- gethtml.sh
+++ gethtml.sh
@@ -10,7 +10,7 @@ i=1
 mkdir $output/3_html
 a=$(cat httprobe.txt|wc -l)
 echo '#!/bin/bash'>getjs1.sh
-echo 'for((i=1;i<'$a';i+=1)); do file=$(head -$i httprobe.txt |tail -1); output=$output; touch $output/3_html/$i.html; curl -L --speed-time 5 --speed-limit 1 $file>>$output/3_html/$i.html; done'>>getjs1.sh
+echo 'for((i=1;i<'$a';i+=1)); do file=$(head -$i httprobe.txt |tail -1); output=$output; touch $output/3_html/$i.html; curl -iL --speed-time 5 --speed-limit 1 $file>>$output/3_html/$i.html; done'>>getjs1.sh
 bash getjs1.sh
 rm getjs1.sh
 
@@ -21,13 +21,13 @@ a=$(cat httprobe.txt|wc -l) ; ac=$((a/4)) ; dc=$((a/2)) ; rock=$((ac*3))
 
 mkdir $output/3_html
 echo '#!/bin/bash'>getjs1.sh
-echo 'for((i=1;i<'$ac';i+=1)); do file=$(head -$i httprobe.txt |tail -1); output=$output; touch $output/3_html/$i.html; curl -L --speed-time 5 --speed-limit 1 $file>>$output/3_html/$i.html; done'>>getjs1.sh
+echo 'for((i=1;i<'$ac';i+=1)); do file=$(head -$i httprobe.txt |tail -1); output=$output; touch $output/3_html/$i.html; curl -iL --speed-time 5 --speed-limit 1 $file>>$output/3_html/$i.html; done'>>getjs1.sh
 echo '#!/bin/bash'>getjs2.sh
-echo 'for((i='$ac';i<'$dc';i+=1)); do file=$(head -$i httprobe.txt |tail -1); output=$output; touch $output/3_html/$i.html; curl -L --speed-time 5 --speed-limit 1 $file>>$output/3_html/$i.html; done'>>getjs2.sh
+echo 'for((i='$ac';i<'$dc';i+=1)); do file=$(head -$i httprobe.txt |tail -1); output=$output; touch $output/3_html/$i.html; curl -iL --speed-time 5 --speed-limit 1 $file>>$output/3_html/$i.html; done'>>getjs2.sh
 echo '#!/bin/bash'>getjs3.sh
-echo 'for((i='$dc';i<'$rock';i+=1)); do file=$(head -$i httprobe.txt |tail -1); output=$output; touch $output/3_html/$i.html; curl -L --speed-time 5 --speed-limit 1 $file>>$output/3_html/$i.html; done'>>getjs3.sh
+echo 'for((i='$dc';i<'$rock';i+=1)); do file=$(head -$i httprobe.txt |tail -1); output=$output; touch $output/3_html/$i.html; curl -iL --speed-time 5 --speed-limit 1 $file>>$output/3_html/$i.html; done'>>getjs3.sh
 echo '#!/bin/bash'>getjs4.sh
-echo 'for((i='$rock';i<'$a';i+=1)); do file=$(head -$i httprobe.txt |tail -1); output=$output; touch $output/3_html/$i.html; curl -L --speed-time 5 --speed-limit 1 $file>>$output/3_html/$i.html; done'>>getjs4.sh
+echo 'for((i='$rock';i<'$a';i+=1)); do file=$(head -$i httprobe.txt |tail -1); output=$output; touch $output/3_html/$i.html; curl -iL --speed-time 5 --speed-limit 1 $file>>$output/3_html/$i.html; done'>>getjs4.sh
 echo '#!/bin/bash'>exe_getjs.sh
 echo 'output=$output'>>exe_getjs.sh
 echo 'bash getjs1.sh'>>exe_getjs.sh
