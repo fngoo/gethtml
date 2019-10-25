7cc46b0c6c99a1c17037077072e2457e4868c630
fngoo <38164287+fngoo@users.noreply.github.com>
2019-10-25 20:20:08 +0800

    Update gethtml.sh

--- gethtml.sh
+++ gethtml.sh
@@ -4,7 +4,7 @@ cd /root/script/3_httprobe
 
 a=$(cat httprobe.txt|wc -l)
 mkdir $output/3_html
-for((i=1;i<$a;i+=1)); do file=$(head -$i httprobe.txt |tail -1) ; echo "touch $output/3_html/$i.html; curl -i -L --speed-time 5 --speed-limit 1 \"$file\" >> $output/3_html/$i.html" >> exe.sh; done
+for((i=1;i<=$a;i+=1)); do file=$(head -$i httprobe.txt |tail -1) ; echo "touch $output/3_html/$i.html; curl -i -L --speed-time 5 --speed-limit 1 \"$file\" >> $output/3_html/$i.html" >> exe.sh; done
 
 cat exe.sh | parallel --jobs 0 --delay 0.1 --retries 1 ; rm exe.sh
 
